3852d5e58988bfe46fa4a7d9c4d290e9793028ed
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-17 12:22:29 +0000

    dataTypes Discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") throw error beacuse in node js there is another way to use alert
+
+// Remember code readability should be high
+
+let name = "Shahzaib"
+let age = 22
+let isLoggedIn = true
+let state;
+
+// dataTypes: 
+// number => has specific range 2^52
+// bigInt
+// string
+// null => standalone value itself
+// undefined => value is not defined
+// boolean
+// symbol => use for unique
+
+// All of the above primitive data types
+
+console.log(typeof name)
+
+console.log(typeof null)
+// typeof null is object
+
+console.log(typeof undefined);
+// typeof undefined is undefined
