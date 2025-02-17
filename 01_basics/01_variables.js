ce82f4c3415dbbdceb1a04e39c6e4b9eebea1903
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-17 11:56:21 +0000

    Variables Discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1449667
+let accountEmail = "skkhan4632@gmail.com"
+var accountPassword = "12345"
+accountcity = "Karachi" // allowed in old tradional code
+
+// accountId = 13 Assignment to constant variable not allowed 
+
+accountEmail = "shahzaib@gmail.com"
+accountPassword = "123456"
+accountcity = "Islamabad"
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountcity])
+
+/* 
+preferd not to use var because of issue in block scope and functional scope
+*/
