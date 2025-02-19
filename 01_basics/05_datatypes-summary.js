7721c5c8f4156be62c1a3fae98162f1e9c9830f9
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-19 19:09:05 +0000

    Memory Discussion

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -21,7 +21,7 @@ let isLoggedIn = false;
 const id = Symbol("1234");
 const anotherId = Symbol("1234");
 // return types of symbol is symbol
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // BigInt
 
@@ -47,3 +47,30 @@ const myFunction = function () {
 
 // type of BigInt is Undefined
 // type of null is object
+
+// ++++++++++++++++ MEMORY +++++++++++++++++++++++++
+
+// STACK MEMORY => use Prmitive datatypes
+
+let myYoutubeName = "Code With SK"
+
+let anotherName = myYoutubeName
+
+anotherName = "Code With SKKHAN"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+// Reference dataytpes uses => Heap Memory
+
+let userOne = {
+  email:"skkhan@gmail.com",
+  country: "Pakistan"
+}
+
+let userTwo = userOne
+
+userTwo.country = 'Germany'
+
+console.log(userOne.country);
+console.log(userTwo.country);
