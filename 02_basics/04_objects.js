7d7da849c9e836f6e4b839856a55e578d75313f4
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-21 19:19:12 +0000

    Objects Discussion

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+// const tinderUser = new Object() // through singleton
+
+const tinderUser = {};
+
+tinderUser.id = "1234abc";
+tinderUser.name = "sam";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Shahzaib",
+      lastname: "Khan",
+    },
+  },
+};
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const object1 = {
+  1: "A",
+  2: "B",
+};
+
+const object2 = {
+  3: "C",
+  4: "D",
+};
+
+// const object3 = {object1,object2}
+// const object3 = Object.assign(object1,object2)
+const object3 = { ...object1, ...object2 };
+// console.log(object3);
+
+const users = [
+    {
+        id:1,
+        email:"shahzaib@gmail.com"
+    },
+    {
+        id:2,
+        email:"shahzaib@yahoo.com"
+    },
+    {
+        id:3,
+        email:"shahzaib@outlook.com"
+    },
+]
+
+// console.log(users[0].id);
+// console.log(users[0].email);
+
+
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('id'))
