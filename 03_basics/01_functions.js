2e581b90af007d3bd919c5a51b718cae336a85fa
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-26 07:47:40 +0000

    Function Discussion

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,35 @@ function loginUserMessage(username = "Sk") {
 }
 
 // console.log(loginUserMessage("Shahzaib"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 1300));
+const user = {
+  username: "shahzaib",
+  price: 1300,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user)
+// handleObject({
+//   username: "Shahzaib Khan",
+//   price: 500,
+// });
+
+const myNewArray = [200, 400, 100, 600];
+
+function handleArray(getArray){
+    return getArray[1]
+}
+
+// console.log(handleArray(myNewArray));
+console.log(handleArray([200, 400, 100, 600]));
