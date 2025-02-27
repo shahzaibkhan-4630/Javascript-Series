ea154f5557d08e8824192efc19d901dd6b2c6acb
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-27 04:46:20 +0000

    Nested Scope Discussion

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,4 @@
-var c = 300
+var c = 300;
 
 if (true) {
   let a = 10;
@@ -8,4 +8,41 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one() {
+  const username = "Shahzaib";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+
+// one()
+
+if (true) {
+  const username = "Shahzaib";
+  if (username === "Shahzaib") {
+    const website = " Youtube";
+    console.log(username + website);
+  }
+  // console.log(website);
+}
+
+// console.log(username)
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+function addOne(value) {
+  return value + 1;
+}
+
+addOne(5);
+
+const addTwo = function (value) {
+  return value + 1;
+};
+
+addTwo(5)
