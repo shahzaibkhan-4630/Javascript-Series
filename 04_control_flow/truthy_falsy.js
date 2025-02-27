76624a08cc2a974c0d0e65234b37c848dc5a904c
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-27 20:41:15 +0000

    Control Flow Discussion End

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -1,12 +1,12 @@
-const userEmail = "skkhan@gmail.com";
+// const userEmail = "skkhan@gmail.com";
 // const userEmail = "" // else statement run because empty string
 // [] truthy value
 
-if (userEmail) {
-  console.log("Got User Email");
-} else {
-  console.log("Don't have user email");
-}
+// if (userEmail) {
+//   console.log("Got User Email");
+// } else {
+//   console.log("Don't have user email");
+// }
 
 // falsy values
 
@@ -25,9 +25,26 @@ if (userEmail) {
 
 // if(Object.keys(emptyObject).length === 0){
 //     console.log("Object Is Empty");
-    
+
 // }
 
 // false == 0 // output true
 // false == '' // output true
 // 0 == '' // output true
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10;
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 25
+// console.log(val1);
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("Less Than 80") : console.log("more than 80");
