f65c66164d2dc9b232abea3a00b93f162260c267
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-27 20:09:39 +0000

    truthy falsy discussion

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -1,8 +1,33 @@
-const userEmail = "skkhan@gmail.com"
+const userEmail = "skkhan@gmail.com";
+// const userEmail = "" // else statement run because empty string
+// [] truthy value
 
-if(userEmail){
-    console.log("Got User Email");
-}else{
-    console.log();
-    
+if (userEmail) {
+  console.log("Got User Email");
+} else {
+  console.log("Don't have user email");
 }
+
+// falsy values
+
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+// rest of other from the above are truthy values
+// Some special truth values below
+// "0","false"," ",{},[],function(){} empty functions
+
+// const array = [];
+
+// if (array.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObject = {}
+
+// if(Object.keys(emptyObject).length === 0){
+//     console.log("Object Is Empty");
+    
+// }
+
+// false == 0 // output true
+// false == '' // output true
+// 0 == '' // output true
