70053ebfc8e88c3d57c18e003aa452952aa47b9e
Shahzaib Khan <134592523+shahzaibkhan-4630@users.noreply.github.com>
2025-02-28 06:53:02 +0000

    While DoWhile Loops Discussion

---
+++ 05_loops/two.js
@@ -0,0 +1,19 @@
+// let index = 1;
+// while (index <= 10) {
+//     console.log(`Value of Index is: ${index}`);
+//   index++;
+// }
+
+// let myArray = ["Flash", "Batman", "Spiderman"];
+// let arr = 0;
+// while (arr < myArray.length) {
+//   console.log(myArray[arr]);
+//   arr++
+// }
+
+let score = 1
+
+do {
+    console.log(`The Value Of Score is ${score}`);
+    score++
+} while (score<=10);
